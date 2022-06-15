1e41dd8ea17175e0100579e726774c5c01b28c44
Sergey Buksha <sergey066@yandex.ru>
2022-06-15 12:25:58 +0300

    .gitignore modified and applied

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,6 +10,7 @@ fun main() {
 }
 
 fun commission(amount: Int): Int {
+    val minCommission = 35_00
     val commission = amount * 75 / 10_000
-    return if (commission < 35_00) 35_00 else commission
+    return if (commission < minCommission) minCommission else commission
 }
