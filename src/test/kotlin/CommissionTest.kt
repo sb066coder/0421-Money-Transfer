38c3e79cadd92a78f797d1d2292439fccd2a4017
Sergey Buksha <sergey066@yandex.ru>
2022-06-16 17:10:16 +0300

    Unit test CommissionTest added

---
+++ src/test/kotlin/CommissionTest.kt
@@ -0,0 +1,27 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class CommissionTest {
+
+    @Test
+    fun VK_PAY_commission() {
+        assertEquals(0, commission(1000_00))
+        assertEquals(0, commission(50_00, 1000_00, accountType = "Another"))
+        assertEquals(0, commission(10_000_00, 1000_00))
+    }
+
+    @Test
+    fun MASTERCARD_commission() {
+        assertEquals(0, commission(8000_00, accountType = MASTERCARD))
+        assertEquals(6800, commission(8000_00, 78000_00, MAESTRO))
+    }
+
+    @Test
+    fun VISA_commission() {
+        assertEquals(3500, commission(1_000_00, accountType =  VISA))
+        assertEquals(37500, commission(50_000_00, accountType =  VISA))
+        assertEquals(63750, commission(85_000_00, accountType =  MIR))
+        assertEquals(63750, commission(85_000_00, accountType =  VISA))
+    }
+}
